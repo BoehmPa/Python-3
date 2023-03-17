1343b19263882b89be4b14250a9300669d0513aa
BoehmPa <pascalboehm2805@gmail.com>
2023-03-17 22:56:08 +0100

    updated

---
+++ Basics/Grunddatentypen/grunddatentypen.py
@@ -0,0 +1,29 @@
+# Ganzzahlen: 
+print()
+print()
+print()
+print()
+## Rechenoperationen
+print()
+print()
+print()
+print()
+
+"""
+# Gleitkommazahlen
+print()
+print()
+print()
+print()
+## Rechenoperationen
+print()
+print()
+print()
+print()
+"""
+
+# Zeichenketten
+print()
+print()
+print()
+print()
